10e4a1264b8989974623bb4eeb6076d4366b0227
unknown <sneharaja148@gmail.com>
2024-01-31 10:37:12 +0530

    final commit

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -2,14 +2,14 @@ export interface Iuser{
     id:number,
     firstname:string,
     lastname:string,
-    
     email:string,
     phone:string,
     address:string,
     password:string,
     business_plan_id:number,
     home_plan_id:number,
-    no_plan_id:number
+    no_plan_id:number,
+    role:string
     
 }
 
